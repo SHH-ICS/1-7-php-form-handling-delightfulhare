292e13588f337ccbd247b4a8ca2b9929dc424d77
Olivia-Steed <148646094+delightfulhare@users.noreply.github.com>
2024-02-28 14:21:41 +0000

    txt

--- welcome.php
+++ welcome.php
@@ -8,13 +8,7 @@
   </head>
 
   <body>
-  <?php
-    $userName = "";
-    if ( isset( $_POST['userName'] ) ){
-      $userName = $_POST['userName'];
-    }
-    echo "<h1>Welcome ".$userName."!</h1>\n";
-  ?>
+  
     
   </body>
   
